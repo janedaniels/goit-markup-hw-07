d4263a0b7e99ddbc951ffe3edbed9c883e1a2b58
janedaniels <126726466+janedaniels@users.noreply.github.com>
2023-05-01 22:06:18 +0300

    HW07_06

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -6,6 +6,7 @@
     };
     refs.openMenuBtn.addEventListener('click', toggleMenu);
     refs.closeMenuBtn.addEventListener('click', toggleMenu);
+    
     function toggleMenu() {
         refs.menu.classList.toggle('is-hidden');
         document.body.classList.toggle('no-scroll');
